e9017cf86c0266d2897ad29ad2e7aded41f5fe1b
Chad Dayton <cdayton96@gmail.com>
2021-04-13 16:49:08 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Text></Text>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25 }}>Fasting Calculator</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: '10%',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
