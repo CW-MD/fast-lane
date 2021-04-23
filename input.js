f491eae9cac93c443d77c75ad7d7137e1e8a6a5b
Chad Dayton <Cdayton96@gmail.com>
2021-04-23 15:40:43 -0500

    fix: refactored input result to use epoch time, and returns a more readable timestamp

--- input.js
+++ input.js
@@ -8,27 +8,27 @@ export default function Input(){
     const curmin = new Date().getMinutes()
     const curDay = new Date().getDate() ;
     const curMonth = new Date().getMonth() + 1
+    var curTime = new Date().getTime();
+    
+    
 
     //let dateStamp = new Date().toLocaleDateString()
 
     const convert = (hours) =>{
-        let dayCount = 0;
-        let hourCount = 0;
-        while(hours >= 24){
-            dayCount++;
-            hours -=24;
-        }
-        hourCount += hours;
+        
+        //convert the given hour to ms, can also be expressed as hours*3600000
+        hours = (hours*60*60*1000)
 
+        const timeToEnd = new Date(hours + curTime).toString()
 
-        console.log(`You can break the fast in ${dayCount} days, ${hourCount} hours`)
+        console.log('can break fast at:' + timeToEnd)
         
     }
 
     
     
-    console.log(curhour, typeof curhour)
-    console.log(curmin)
+    //console.log(curhour, typeof curhour)
+    //console.log(curmin)
     return(
         <>
         <Text>The Current Date is {curMonth}/{curDay} at {curhour}:{curmin}</Text>
